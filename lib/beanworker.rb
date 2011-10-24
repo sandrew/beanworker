c861d2c6db724960ee871ac7b9e25e4a6afc1b7b
Andrew Shaydurov <gearhead@inbox.ru>
2011-10-24 13:56:46 +1100

    error logging added

--- lib/beanworker.rb
+++ lib/beanworker.rb
@@ -4,7 +4,7 @@ require 'beanqueue'
 require File.dirname(__FILE__) + '/beanworker/worker'
 
 module Beanworker
-  VERSION = '0.0.2'
+  VERSION = '0.0.3'
 
   class << self
     attr_accessor :connection_config
--- lib/beanworker/worker.rb
+++ lib/beanworker/worker.rb
@@ -9,7 +9,8 @@ module Beanworker
       num.times do
         Thread.new do
           connection = Beanqueue.connect Beanworker.connection_config
-          tubes.each { |tube| connection.watch(tube.gsub('_', '.')) }
+          tubes.each { |tube| connection.watch(tube.to_s.gsub('_', '.')) }
+          logger.info("Listening: #{tubes.inspect}") if logger
           loop do
             get_one_job connection
           end
@@ -23,15 +24,19 @@ module Beanworker
       Thread.new do
         loop do
           sleep(at - Time.now)
-          Timeout.timeout timeout_secs do
-            self.send(name, *args)
-          end
+          work_job(name, timeout_secs, args, @need_fork)
           at += frequency
         end
       end
     end
 
-    private
+    def logger=(l)
+      @logger = l
+    end
+
+    def fork_default(val)
+      @need_fork = val
+    end
 
     def get_one_job(connection)
       job = connection.reserve
@@ -41,7 +46,8 @@ module Beanworker
       job.delete
     rescue SystemExit
       raise
-    rescue
+    rescue => e
+      logger.error(e.backtrace.unshift(e.message)) if logger
       job.bury rescue nil
     end
 
@@ -61,10 +67,6 @@ module Beanworker
       end
     end
 
-    def fork_default(val)
-      @need_fork = val
-    end
-
     def make_first_schedule(frequency, at)
       if at && (frequency % (24*3600) == 0)
         t = Time.parse at
@@ -72,6 +74,10 @@ module Beanworker
       else
         Time.now + frequency
       end
+    end
+
+    def logger
+      @logger ||= Logger.new(STDOUT)
     end
   end
 end
